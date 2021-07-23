c734f236743f66f09394c126ed581d28cdcf77fa
lisa-schroeder <85608997+lisa-schroeder@users.noreply.github.com>
2021-07-23 15:42:14 +0200

    Update chemicals.py

--- experiment/chemicals.py
+++ experiment/chemicals.py
@@ -19,18 +19,18 @@ class chemical():
         self.molarMass = molarMass
 
 
-def getChemicalsList(dataFile):         #input: .csv-file, output: dict
+def getChemicalsList(dataFile):         # input: .csv-file, output: dict
     # This function loads the .csv database including the information regarding the chemicals and generates objects of the chemicals class. A dictionary containing all the chemicals listed in the database is returned using the NameShort in the database as keys.
     chemInfo = pd.read_csv(dataFile, sep=";")       
     chemList = {}
-    for i in chemInfo.index:        #write each chemical as object into the dict
+    for i in chemInfo.index:        # write each chemical as object into the dict
         chem = chemical(chemInfo.loc[i, "NameShort"], chemInfo.loc[i, "NameLong"], chemInfo.loc[i, "Density at 20 degreeC / g/cm3"], chemInfo.loc[i, "Molar mass / g/mol"])
         chemList[chemInfo.loc[i, "NameShort"]] = chem
     # save the chemList dictionary in order to be able to load once it is generated. It does not need to be newly generated, if no changes in the database occured.
     SaveToFile("chemList", chemList)
     return chemList
 
-def loadChemicalsList(file_chemList):   #input: path to pickle file
+def loadChemicalsList(file_chemList):   # input: path to pickle file
     # This function loads a saved chemList object.
     load_file = open(file_chemList, 'rb')
     out = pickle.load(load_file)
